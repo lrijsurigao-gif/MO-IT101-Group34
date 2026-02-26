df1484fb3e00e59cd9bb1a9f367e35252405783e
lrijsurigao-gif <lr.ijsurigao@mmdc.mcl.edu.ph>
2026-02-27 05:24:32 +0800

    2.1.2026

---
+++ src/main/java/com/mycompany/mo/it101/group34/MOIT101Group34.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mo.it101.group34;
+
+/**
+ * 
+ Repository Name: MO-IT101-Group34 
+ Date Added: 1/5/2022
+ */
+public class MOIT101Group34 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
