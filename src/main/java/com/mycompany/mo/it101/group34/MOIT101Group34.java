e41347eba683135b2fa85dad51a91e9fbfed341e
Ioshua Surigao <>
2026-03-09 21:26:14 +0800

    Welcome Output While true loop for easier not match employee Try Catch Exception Output outside of while loop for cleaner IDE

--- src/main/java/com/mycompany/mo/it101/group34/MOIT101Group34.java
+++ src/main/java/com/mycompany/mo/it101/group34/MOIT101Group34.java
@@ -6,6 +6,8 @@ package com.mycompany.mo.it101.group34;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+
 import java.time.Duration;
 import java.time.LocalTime;
 import java.time.YearMonth;
@@ -24,23 +26,65 @@ import java.util.Scanner;
 public class MOIT101Group34 {
 
     public static void main(String[] args) {
-        // read both CSV files
-        String employeeCSV = "MO - IT101 - Group34/MotorPH_Employee Data - Employee Details.csv";
-        String attendanceCSV = "MO - IT101 - Group34/MotorPH_Employee Data - Attendance Record.csv";
+        // declare both CSV files
+        String employeeCSV = "C:/Users/Kat/Documents/NetBeansProjects/MO-IT101-Group34/MotorPH_Employee Data - Employee Details.csv";
+        String attendanceCSV = "C:/Users/Kat/Documents/NetBeansProjects/MO - IT101 - Group34/MotorPH_Employee Data - Attendance Record.csv";
         
         // declaration of variables
         String employeeNum = "";
-        String f_name = "";
         String l_name = "";
+        String f_name = "";
         String birthday = "";
         boolean match = false;
         
-        // output
-        System.out.println("\n===================================");
-        System.out.println("Employee # : " + employeeNum);
-        System.out.println("Employee Name : " + f_name + " " + l_name);
-        System.out.println("Birthday : " + birthday);
+        // Welcome Output
         System.out.println("===================================");
+        System.out.println("Welcome to MotorPH Payroll System");
+        System.out.println("===================================\n");
         
+        while(true){
+            match = false;
+            // Ask for user input
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Employee No.: ");
+            String inputEmpNum = scanner.nextLine();
+
+            // used TRY CATCH EXCEPTION because of errors without one
+            try {
+                BufferedReader bReader = new BufferedReader(new FileReader(employeeCSV));
+
+                // reads header row
+                bReader.readLine();
+                String line;
+
+                while((line=bReader.readLine()) != null){
+                    String[] data = line.split(","); // split the rows into columns
+                    if (data[0].equals(inputEmpNum)) // data[0] firstColumn EmployeeNum 
+                    {
+                        employeeNum = data[0]; // Column 1: EmployeeNum
+                        l_name = data[1]; // Column 2: LastName
+                        f_name = data[2]; // Column 3: FirstName
+                        birthday = data[3]; // Column 4: Birthday
+                        match = true; // Change match to true if data[0] have match
+                    }
+                }
+            }
+
+            catch (IOException e){ // print error message
+                System.out.println("Error Reading File: " + e.getMessage());
+            }
+            if(match){
+                break;
+            }
+            else{
+                System.out.println("Employee not found. Please try again.");
+            }
+        }
+            // output outside of while
+            System.out.println("===================================");
+            System.out.println("Employee # : " + employeeNum);
+            System.out.println("Employee Name : " + f_name + " " + l_name);
+            System.out.println("Birthday : " + birthday);
+            System.out.println("===================================");
     }
 }
