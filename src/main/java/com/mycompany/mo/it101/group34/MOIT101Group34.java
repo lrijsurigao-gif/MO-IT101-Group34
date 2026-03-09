75c5da0836bb4f75f78d04d327360e73a1617b62
Ioshua Surigao <>
2026-03-09 20:33:47 +0800

    Declaration of Variables + Output

--- src/main/java/com/mycompany/mo/it101/group34/MOIT101Group34.java
+++ src/main/java/com/mycompany/mo/it101/group34/MOIT101Group34.java
@@ -4,6 +4,14 @@
 
 package com.mycompany.mo.it101.group34;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 /**
  * 
  Repository Name: MO-IT101-Group34 
@@ -11,12 +19,28 @@ package com.mycompany.mo.it101.group34;
  
  Group Members:
  * Ioshua Jericho Surigao
- * Irish Chene Sia
  * Jan Braunel Angeles
  */
 public class MOIT101Group34 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // read both CSV files
+        String employeeCSV = "MO - IT101 - Group34/MotorPH_Employee Data - Employee Details.csv";
+        String attendanceCSV = "MO - IT101 - Group34/MotorPH_Employee Data - Attendance Record.csv";
+        
+        // declaration of variables
+        String employeeNum = "";
+        String f_name = "";
+        String l_name = "";
+        String birthday = "";
+        boolean match = false;
+        
+        // output
+        System.out.println("\n===================================");
+        System.out.println("Employee # : " + employeeNum);
+        System.out.println("Employee Name : " + f_name + " " + l_name);
+        System.out.println("Birthday : " + birthday);
+        System.out.println("===================================");
+        
     }
 }
